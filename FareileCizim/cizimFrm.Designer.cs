97aa4a4cc4e2078c3455231cb27456840b22282f
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2024-01-04 14:13:38 +0300

    Proje dosyası ekle.

---
+++ FareileCizim/cizimFrm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace FareileCizim
+{
+    partial class cizimFrm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // cizimFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 589);
+            this.Name = "cizimFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Çizim";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.cizimFrm_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ FareileCizim/cizimFrm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FareileCizim
+{
+    public partial class cizimFrm : Form
+    {
+        int x1,y1, x2, y2,t=0;
+        Graphics g;
+        Pen p = new Pen(Color.Red, 3);
+        public cizimFrm()
+        {
+            InitializeComponent();
+            g=this.CreateGraphics();
+        }
+
+        private void cizimFrm_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(t==0)
+            {
+                x1 = e.X;
+                y1 = e.Y;
+                t = 1;
+            }
+            else
+            {
+                x2=e.X;
+                y2=e.Y;
+                t = 0;
+                g.DrawLine(p, x1, y1, x2, y2);
+            }           
+        }
+    }
+}
